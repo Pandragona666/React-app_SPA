bcae5573214c12bdeb34ec16ce1352116f21c71e
joannakordasecom <joanna.kordas@ecom.software>
2021-05-22 22:48:12 +0200

    Updates

---
+++ precache-manifest.230d0fbc1ef235ab14c63bf4ca363014.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e07f37e9d7117b9c706",
+    "url": "/React-app_SPA/static/css/main.ef889611.chunk.css"
+  },
+  {
+    "revision": "0e07f37e9d7117b9c706",
+    "url": "/React-app_SPA/static/js/main.e1b8f8fc.chunk.js"
+  },
+  {
+    "revision": "ab6581e2598bc42c566f",
+    "url": "/React-app_SPA/static/js/runtime~main.98c7f3b5.js"
+  },
+  {
+    "revision": "ff1aceb07e0f333561f3",
+    "url": "/React-app_SPA/static/js/2.99e725a1.chunk.js"
+  },
+  {
+    "revision": "9090c3049f241c9a8814f229e6c80c7e",
+    "url": "/React-app_SPA/static/media/header1.9090c304.jpg"
+  },
+  {
+    "revision": "46169cda1caac12c3461004495b46a64",
+    "url": "/React-app_SPA/static/media/header2.46169cda.jpg"
+  },
+  {
+    "revision": "424fc01804981b44da6743ee78907735",
+    "url": "/React-app_SPA/static/media/header3.424fc018.jpg"
+  },
+  {
+    "revision": "c3b95639de23815a1fa40638768fc2d3",
+    "url": "/React-app_SPA/static/media/header4.c3b95639.jpg"
+  },
+  {
+    "revision": "4961d690fc78bdfa87027c834c2180ff",
+    "url": "/React-app_SPA/static/media/header5.4961d690.jpg"
+  },
+  {
+    "revision": "04a16d7d8146664db052cb99a3ea41b9",
+    "url": "/React-app_SPA/index.html"
+  }
+];
